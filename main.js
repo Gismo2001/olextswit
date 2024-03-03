58a1d877f35425fdb33efdf02355555baa6a9d00
Gismo2001 <rankenpunk@googlemail.com>
2024-03-03 12:24:27 +0100

    alle layer hinzugefügt

--- main.js
+++ main.js
@@ -64,8 +64,98 @@ function removeTempMarker() {
   });
 }
 
+const attribution = new ol.control.Attribution({
+  collapsible: true
+});
 
-//Style für sle
+
+const gew_infoStyle = new ol.style.Style({
+  stroke: new ol.style.Stroke({
+    color: 'rgba(209, 100, 253, 1)',
+    width: 3
+  }),
+});
+
+const son_linStyle = new ol.style.Style({
+  stroke: new ol.style.Stroke({
+    color: 'rgba(209, 32, 253, 1)',
+    width: 4
+  }),
+});
+
+
+const son_punStyle = new ol.style.Style({
+  image: new ol.style.RegularShape({
+    fill: new ol.style.Fill({color:'rgba(209, 32, 253, 1)' }),
+    stroke: new ol.style.Stroke({
+      color: 'black',
+      width: 2
+    }),
+    points: 4,
+    radius: 7,
+    angle: Math.PI / 4
+  })
+});
+  
+const queStyle = new ol.style.Style({
+  image: new ol.style.RegularShape({
+    fill: new ol.style.Fill({color:'rgba(209, 32, 253, 1'}),
+    stroke: new ol.style.Stroke({
+      color: 'black',
+      width: .5
+    }),
+    points: 4,
+    radius: 7,
+    angle: Math.PI / 2
+  })
+});
+  
+const dueStyle = new ol.style.Style({
+  image: new ol.style.RegularShape({
+    fill: new ol.style.Fill({color:'rgba(209, 32, 253, 1'}),
+    stroke: new ol.style.Stroke({
+      color: 'black',
+      width: 2
+    }),
+    points: 4,
+    radius: 7,
+    angle: Math.PI / 4
+  })
+});
+  
+const wehStyle = new ol.style.Style({
+  image: new ol.style.RegularShape({
+    fill: new ol.style.Fill({color: 'green'}),
+    stroke: new ol.style.Stroke({
+      color: 'black',
+      width: 2
+    }),
+    points: 3,
+    radius: 7,
+    rotation: 0  // Setzen Sie die Rotation auf 0 für ein Dreieck
+  })
+});
+  
+const bru_nlwknStyle = new ol.style.Style({
+  image: new ol.style.RegularShape({
+    fill: new ol.style.Fill({color: 'blue'}),
+    stroke: new ol.style.Stroke({color: 'grey', width: 1}),
+    points: 4,
+    radius: 7,
+    angle: Math.PI / 4
+  })
+});
+  
+const bru_andereStyle = new ol.style.Style({
+  image: new ol.style.RegularShape({
+    fill: new ol.style.Fill({color:'rgba(100, 100, 100, 1)'}),
+    stroke: new ol.style.Stroke({color: 'grey',width: 1}),
+    points: 4,
+    radius: 6,
+    angle: Math.PI / 4
+  })
+});
+  
 const sleStyle = new ol.style.Style({
   image: new ol.style.RegularShape({
     fill: new ol.style.Fill({color: 'red'}),
@@ -78,11 +168,87 @@ const sleStyle = new ol.style.Style({
     angle: Math.PI / 4
   })
 });
-
-
-const attribution = new ol.control.Attribution({
-  collapsible: true
+  
+const km10scalStyle = new ol.style.Style({
+  stroke: new ol.style.Stroke({
+    color: 'grey',
+    width: .5
+  })
 });
+   
+function getStyleForArtEin(feature) {   
+  const artValue = feature.get('Ein_ord');
+  let fillColor, strokeColor;
+  switch (artValue) {
+      case '1. Ordnung':
+        fillColor = 'rgba(0, 68, 255, .8)';
+        strokeColor = 'black';
+        break;
+      case '2. Ordnung':
+        fillColor = 'rgba(214, 0, 0, .8)';
+        strokeColor = 'black';
+        break;
+      case '3. Ordnung':
+        fillColor = 'rgba(114, 114, 114, .8)';
+        strokeColor = 'black';
+        break;
+      case 'Sonstige':
+        fillColor = 'rgba(27, 117, 0, .8)';
+        strokeColor = 'black';
+        break;
+      default:
+        fillColor = 'grey';
+        strokeColor = 'grey';
+    }
+    return new ol.style.Style({
+      image: new ol.style.Circle({
+          fill: new ol.style.Fill({
+            color: fillColor
+          }),
+          stroke: new ol.style.Stroke({
+            color: strokeColor,
+            width: 0.5
+          }),
+          radius: 7
+        })
+      
+    })
+}
+
+//Berechnung Style für FSK
+function getStyleForArtFSK(feature) {
+const artValue = feature.get('Art');
+let fillColor, strokeColor;
+
+switch (artValue) {
+  case 'p':
+    fillColor = 'rgba(200, 200, 200, .6)';
+    strokeColor = 'black';
+    break;
+  case 'o':
+    fillColor = 'rgba(255, 220, 220, .6)';
+    strokeColor = 'black';
+    break;
+  case 'l':
+    fillColor = 'rgba(255, 190, 150, .6)';
+    strokeColor = 'black';
+    break;
+  default:
+    fillColor = 'rgba(255, 255, 255, 1)';
+    strokeColor = 'grey';
+}
+
+return new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: fillColor
+  }),
+  stroke: new ol.style.Stroke({
+    color: strokeColor,
+    width: 0.5
+  })
+});
+}
+
 
 const mapView = new ol.View({
   center: ol.proj.fromLonLat([7.35, 52.7]),
@@ -95,6 +261,219 @@ const map = new ol.Map({
   controls: ol.control.defaults().extend([attribution])
   
 });
+
+
+
+const exp_allgm_fsk_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({format: new ol.format.GeoJSON(), url: function (extent) {return './myLayers/exp_allgm_fsk.geojson' + '?bbox=' + extent.join(','); }, strategy: ol.loadingstrategy.bbox }),
+  title: 'fsk', // Titel für den Layer-Switcher
+  style: getStyleForArtFSK,
+  visible: false,
+  minResolution: 0,
+  maxResolution: 4
+})
+
+// exp_gew_info
+const exp_gew_info_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+  format: new ol.format.GeoJSON(),
+  url: function (extent) {return './myLayers/exp_gew_info.geojson' + '?bbox=' + extent.join(','); }, strategy: ol.loadingstrategy.bbox }),
+  title: 'gew_info', // Titel für den Layer-Switcher
+  style: gew_infoStyle,
+  visible: false
+});
+
+// sonstige Linien
+const exp_bw_son_lin_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+  format: new ol.format.GeoJSON(),
+  url: function (extent) {return './myLayers/exp_bw_son_lin.geojson' + '?bbox=' + extent.join(','); }, strategy: ol.loadingstrategy.bbox }),
+  title: 'son_lin', // Titel für den Layer-Switcher
+  style: son_linStyle,
+  visible: false
+});
+
+// sonstige Punkte
+const exp_bw_son_pun_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+  format: new ol.format.GeoJSON(),
+  url: function (extent) {return './myLayers/exp_bw_son_pun.geojson' + '?bbox=' + extent.join(','); }, strategy: ol.loadingstrategy.bbox }),
+  title: 'son_pun', // Titel für den Layer-Switcher
+  style: son_punStyle,
+  visible: false
+});
+
+// ein
+const exp_bw_ein_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+  format: new ol.format.GeoJSON(),
+  url: function (extent) {return './myLayers/exp_bw_ein.geojson' + '?bbox=' + extent.join(','); }, strategy: ol.loadingstrategy.bbox }),
+  title: 'ein', // Titel für den Layer-Switcher
+  style: getStyleForArtEin,
+  visible: false
+});
+
+// que
+const exp_bw_que_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: function (extent) {
+      return './myLayers/exp_bw_que.geojson' + '?bbox=' + extent.join(',');
+    },
+    strategy: ol.loadingstrategy.bbox
+  }),
+  title: 'que', // Titel für den Layer-Switcher
+  style: queStyle,
+  visible: false
+});
+
+// due
+const exp_bw_due_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: function (extent) {
+      return './myLayers/exp_bw_due.geojson' + '?bbox=' + extent.join(',');
+    },
+    strategy: ol.loadingstrategy.bbox
+  }),
+  title: 'due', // Titel für den Layer-Switcher
+  style: dueStyle,
+  visible: false
+});
+
+// weh
+const exp_bw_weh_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: function (extent) {
+      return './myLayers/exp_bw_weh.geojson' + '?bbox=' + extent.join(',');
+    },
+    strategy: ol.loadingstrategy.bbox
+  }),
+  title: 'weh', // Titel für den Layer-Switcher
+  style: wehStyle,
+  visible: false
+});
+
+//bru nlwkn
+const exp_bw_bru_nlwkn_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({format: new ol.format.GeoJSON(), url: function (extent) {return './myLayers/exp_bw_bru_nlwkn.geojson' + '?bbox=' + extent.join(','); }, strategy: ol.loadingstrategy.bbox }),
+  title: 'bru_nlwkn', // Titel für den Layer-Switcher
+  style: bru_nlwknStyle,
+  visible: false
+});
+
+//bru andere
+const exp_bw_bru_andere_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({format: new ol.format.GeoJSON(), url: function (extent) {return './myLayers/exp_bw_bru_andere.geojson' + '?bbox=' + extent.join(','); }, strategy: ol.loadingstrategy.bbox }),
+  title: 'bru_andere', // Titel für den Layer-Switcher
+  style: bru_andereStyle,
+  visible: false
+});
+
+//sle
+const exp_bw_sle_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: function (extent) {
+      return './myLayers/exp_bw_sle.geojson' + '?bbox=' + extent.join(',');
+    },
+    strategy: ol.loadingstrategy.bbox
+  }),
+  title: 'sle', // Titel für den Layer-Switcher
+  style: sleStyle,
+  visible: true
+});
+
+//kilometrierung 10 m
+const km10scal_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({format: new ol.format.GeoJSON(), url: function (extent) {return './myLayers/km_10_scal.geojson' + '?bbox=' + extent.join(','); }, strategy: ol.loadingstrategy.bbox }),
+  title: 'km10scal', // Titel für den Layer-Switcher
+  style: km10scalStyle,
+  visible: true,
+  minResolution: 0,
+  maxResolution: 1 
+});
+
+
+//kilometrierung 100 m
+const km100scal_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({format: new ol.format.GeoJSON(), url: function (extent) {return './myLayers/km_100_scal.geojson' + '?bbox=' + extent.join(','); }, strategy: ol.loadingstrategy.bbox }),
+  title: 'km100scal', // Titel für den Layer-Switcher
+  style: function(feature, resolution) {
+    return km100scalStyle(feature, feature.get('TextString'), resolution);
+  },
+  visible: true,
+  minResolution: 0,
+  maxResolution: 3 
+});
+
+// km 100 Style-Funktion mit Beschriftung
+const km100scalStyle = function(feature, text, resolution) {
+  var minResolution = 0;
+  var maxResolution = 5; 
+  if (resolution > minResolution && resolution < maxResolution) {
+    return new ol.style.Style({
+      text: new ol.style.Text({
+        text: text,
+        font: 'normal 18px "Arial Light", "Helvetica Neue Light", Arial, sans-serif',
+        offsetX: -10,
+        offsetY: 10,
+        fill: new ol.style.Fill({
+          color: 'rgba(128, 128, 128, 1)' // Graue Farbe
+        }),
+        stroke: new ol.style.Stroke({
+          color: '#000000',
+          width: 0.25
+        })
+      })
+    });
+  } else {
+    return null;
+  }
+};
+
+//kilometrierung 500 m
+const km500scal_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({format: new ol.format.GeoJSON(), url: function (extent) {return './myLayers/km_500_scal.geojson' + '?bbox=' + extent.join(','); }, strategy: ol.loadingstrategy.bbox }),
+  title: 'km500scal', // Titel für den Layer-Switcher
+  style: function(feature, resolution) {
+    return km500scalStyle(feature, feature.get('TextString'), resolution);
+  },
+  visible: true,
+  minResolution: 0,
+  maxResolution: 10 
+});
+
+// Style-Funktion mit Beschriftung
+const km500scalStyle = function(feature, text, resolution) {
+  var minResolution = 0;
+  var maxResolution = 10; 
+  if (resolution > minResolution && resolution < maxResolution) {
+    return new ol.style.Style({
+      text: new ol.style.Text({text: text, font: 'normal 20px "Arial Light", "Helvetica Neue Light", Arial, sans-serif', offsetX: -10, offsetY: 10, fill: new ol.style.Fill({color: 'rgba(0, 0, 0, 1)'}),
+      stroke: new ol.style.Stroke({color: '#000000', width: .25 })
+      })
+    });
+  } else {
+    return null;
+  }
+};
+
+//gew Layer
+const gew_layer_layer = new ol.layer.Vector({
+  source: new ol.source.Vector({format: new ol.format.GeoJSON(), url: function (extent) {return './myLayers/gew.geojson' + '?bbox=' + extent.join(','); }, strategy: ol.loadingstrategy.bbox }),
+  title: 'gew', // Titel für den Layer-Switcher
+  name: 'gew',
+  style: new ol.style.Style({
+    fill: new ol.style.Fill({ color: 'rgba(0,28, 240, 0.4)' }),
+    stroke: new ol.style.Stroke({ color: 'blue', width: 2 })
+  })
+})
+
+
+
+
 
 const layerSwitcher = new ol.control.LayerSwitcher({ });
 map.addControl(layerSwitcher);
@@ -203,20 +582,6 @@ button.addEventListener('click', function() {
 // Starte die Positionsupdates, wenn die Seite geladen wird
 //startTracking();
 
-// sle
-const exp_bw_sle_layer = new ol.layer.Vector({
-  source: new ol.source.Vector({
-      format: new ol.format.GeoJSON(),
-      url: function (extent) {
-          return './myLayers/exp_bw_sle.geojson' + '?bbox=' + extent.join(',');
-      },
-      strategy: ol.loadingstrategy.bbox
-  }),
-  title: 'Schleuse',
-  style: sleStyle,
-  visible: true
-});
-
 const wmsHydErstOrdLayer = new ol.layer.Tile({
   source: new ol.source.TileWMS({
     url: 'https://www.umweltkarten-niedersachsen.de/arcgis/services/Hydro_wms/MapServer/WMSServer',
@@ -264,6 +629,129 @@ const wmsHydDrittOrdLayer = new ol.layer.Tile({
   maxResolution: 6
 });
 
+const gnAtlas2023 = new ol.layer.Tile({
+  source: new ol.source.TileWMS(({
+      url: "https://geo.grafschaft.de/arcgis/services/Migratrion_Okt_2020/BAS_Luftbilder_2/MapServer/WMSServer",
+      attributions: ' ',
+     params: {"LAYERS": "10", "TILED": "true", "VERSION": "1.3.0"},
+    })),
+  title: "2023",
+  opacity: 1.000000,
+  visible: false,
+});
+
+const gnAtlas2020 = new ol.layer.Tile({
+  source: new ol.source.TileWMS(({
+      url: "https://geo.grafschaft.de/arcgis/services/Migratrion_Okt_2020/BAS_Luftbilder_2/MapServer/WMSServer",
+      attributions: ' ',
+     params: {"LAYERS": "9", "TILED": "true", "VERSION": "1.3.0"},
+    })),
+  title: "2020",
+  opacity: 1.000000,
+  visible: false,
+});
+
+const gnAtlas2017 = new ol.layer.Tile({
+  source: new ol.source.TileWMS(({
+      url: "https://geo.grafschaft.de/arcgis/services/Migratrion_Okt_2020/BAS_Luftbilder_2/MapServer/WMSServer",
+      attributions: ' ',
+     params: {"LAYERS": "8", "TILED": "true", "VERSION": "1.3.0"},
+    })),
+  title: "2017",
+  opacity: 1.000000,
+  visible: false,
+});
+
+const gnAtlas2014 = new ol.layer.Tile({
+  source: new ol.source.TileWMS(({
+      url: "https://geo.grafschaft.de/arcgis/services/Migratrion_Okt_2020/BAS_Luftbilder_2/MapServer/WMSServer",
+      attributions: ' ',
+     params: {"LAYERS": "7", "TILED": "true", "VERSION": "1.3.0"},
+    })),
+  title: "2014",
+  opacity: 1.000000,
+  visible: false,
+});
+
+const gnAtlas2012 = new ol.layer.Tile({
+  source: new ol.source.TileWMS(({
+      url: "https://geo.grafschaft.de/arcgis/services/Migratrion_Okt_2020/BAS_Luftbilder_2/MapServer/WMSServer",
+      attributions: ' ',
+     params: {"LAYERS": "6", "TILED": "true", "VERSION": "1.3.0"},
+    })),
+  title: "2012",
+  opacity: 1.000000,
+  visible: false,
+});
+
+const gnAtlas2010 = new ol.layer.Tile({
+  source: new ol.source.TileWMS(({
+      url: "https://geo.grafschaft.de/arcgis/services/Migratrion_Okt_2020/BAS_Luftbilder_2/MapServer/WMSServer",
+      attributions: ' ',
+     params: {"LAYERS": "5", "TILED": "true", "VERSION": "1.3.0"},
+    })),
+  title: "2010",
+  opacity: 1.000000,
+  visible: false,
+});
+
+const gnAtlas2009 = new ol.layer.Tile({
+  source: new ol.source.TileWMS(({
+      url: "https://geo.grafschaft.de/arcgis/services/Migratrion_Okt_2020/BAS_Luftbilder_2/MapServer/WMSServer",
+      attributions: ' ',
+     params: {"LAYERS": "4", "TILED": "true", "VERSION": "1.3.0"},
+    })),
+  title: "2009",
+  opacity: 1.000000,
+  visible: false,
+});
+
+const gnAtlas2002 = new ol.layer.Tile({
+  source: new ol.source.TileWMS(({
+      url: "https://geo.grafschaft.de/arcgis/services/Migratrion_Okt_2020/BAS_Luftbilder_2/MapServer/WMSServer",
+      attributions: ' ',
+     params: {"LAYERS": "3", "TILED": "true", "VERSION": "1.3.0"},
+    })),
+  title: "2002",
+  opacity: 1.000000,
+  visible: false,
+});
+
+const gnAtlas1970 = new ol.layer.Tile({
+  source: new ol.source.TileWMS(({
+      url: "https://geo.grafschaft.de/arcgis/services/Migratrion_Okt_2020/BAS_Luftbilder_2/MapServer/WMSServer",
+      attributions: ' ',
+     params: {"LAYERS": "2", "TILED": "true", "VERSION": "1.3.0"},
+    })),
+  title: "1970",
+  opacity: 1.000000,
+  visible: false,
+});
+
+const gnAtlas1957 = new ol.layer.Tile({
+  source: new ol.source.TileWMS(({
+      url: "https://geo.grafschaft.de/arcgis/services/Migratrion_Okt_2020/BAS_Luftbilder_2/MapServer/WMSServer",
+      attributions: ' ',
+     params: {"LAYERS": "1", "TILED": "true", "VERSION": "1.3.0"},
+    })),
+  title: "1957",
+  opacity: 1.000000,
+  visible: false,
+});
+
+const gnAtlas1937 = new ol.layer.Tile({
+  source: new ol.source.TileWMS(({
+      url: "https://geo.grafschaft.de/arcgis/services/Migratrion_Okt_2020/BAS_Luftbilder_2/MapServer/WMSServer",
+      attributions: ' ',
+     params: {"LAYERS": "0", "TILED": "true", "VERSION": "1.3.0"},
+    })),
+  title: "1937",
+  opacity: 1.000000,
+  visible: false,
+});
+
+
+
 
 var dop20ni_layer = new ol.layer.Tile({
   title: "DOP20 NI",
@@ -309,14 +797,40 @@ const osmTile = new ol.layer.Tile({
   }),
 });
 
+
+
+
 const wmsLayerGroup = new ol.layer.Group({
-title: "wms-Layer",
+title: "WMS-Lay",
 fold: true,
 fold: 'close',
 layers: [wmsHydDrittOrdLayer, wmsHydZweitOrdLayer, wmsHydErstOrdLayer]
 });
 
 wmsLayerGroup.setVisible(false);
+
+const GNAtlasGroup = new ol.layer.Group({
+  title: "GN-DOP's",
+  fold: true,
+  fold: 'close',
+  layers: [ gnAtlas2023, gnAtlas2020, gnAtlas2017, gnAtlas2014, gnAtlas2012, gnAtlas2010, gnAtlas2009, gnAtlas2002, gnAtlas1970, gnAtlas1957, gnAtlas1937]
+  });
+  
+wmsLayerGroup.setVisible(false);
+
+const BwGroup = new ol.layer.Group({
+  title: "Bauw.",
+  fold: true,
+  fold: 'close',  
+  layers: [exp_gew_info_layer, exp_bw_son_lin_layer, exp_bw_son_pun_layer, exp_bw_ein_layer, exp_bw_bru_andere_layer, exp_bw_bru_nlwkn_layer, exp_bw_que_layer, exp_bw_due_layer, exp_bw_weh_layer, exp_bw_sle_layer]
+});
+
+const kmGroup = new ol.layer.Group({
+  title: "Station",
+  fold: true,
+  fold: 'close',
+  layers: [km10scal_layer, km100scal_layer, km500scal_layer]
+});
 
 const BaseGroup = new ol.layer.Group({
   title: "Base",
@@ -326,5 +840,9 @@ const BaseGroup = new ol.layer.Group({
 });
 
 map.addLayer(BaseGroup);
+map.addLayer(GNAtlasGroup);
+map.addLayer(exp_allgm_fsk_layer);
+map.addLayer(gew_layer_layer);
 map.addLayer(wmsLayerGroup);
-map.addLayer(exp_bw_sle_layer);
+map.addLayer(kmGroup);
+map.addLayer(BwGroup);
